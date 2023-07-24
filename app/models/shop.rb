896bffeede54592b3da8755e409cfd1f2668afb3
nakaty2023 <nakaty2023@example.com>
2023-07-24 14:50:36 +0900

    商品モデルの作成

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,4 @@
 class Shop < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :address, presence: true, length: { maximum: 255 }
+  validates :name,  presence: true
+  validates :address, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_23_122649) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_054326) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.string "address"
