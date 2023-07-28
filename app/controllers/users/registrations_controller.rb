c7d0206ee4bb2b332d4ac1f37f37aa70bea74d52
nakaty2023 <nakaty2023@example.com>
2023-07-28 22:17:21 +0900

    ユーザー編集機能の実装等 ・ユーザー名、性別及び年齢が編集可能となるよう、ユーザー編集ページを修正 ・application_controller.rbに記載していたbefor_actionをregistrations_controller.rbへ移動 ・ユーザー編集時に許可するパラメータの設定 ・ユーザー登録後にリダイレクト遷移するページの設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :convert_gender_to_integer, only: %i[create update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -10,10 +11,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  def create
-    convert_gender_to_integer
-    super
-  end
+  # def create
+  #   super
+  # end
 
   # GET /resource/edit
   # def edit
@@ -45,22 +45,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
     params[:user][:gender] = params[:user][:gender].to_i if params[:user][:gender].present?
   end
 
-  # protected
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name gender age])
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name gender age])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
