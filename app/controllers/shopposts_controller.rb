f41e4dbd62b29b7619d8a99b908b4dbc34b1d06d
nakaty2023 <nakaty2023@example.com>
2023-08-11 21:29:17 +0900

    店舗に関する投稿を削除する機能の実装 ・Shoppostsコントローラーのdestroyアクション作成 ・ログイン済みユーザーが自分の投稿のみ削除できるよう店舗詳細ページを修正 ・他人の投稿を削除できないよう、また、ログイン済みユーザーのみ投稿を削除できるようフィルターを作成

--- app/controllers/shopposts_controller.rb
+++ app/controllers/shopposts_controller.rb
@@ -1,4 +1,7 @@
 class ShoppostsController < ApplicationController
+  before_action :authenticate_user!, only: :destroy
+  before_action :correct_user, only: :destroy
+
   def create
     @shoppost = if user_signed_in?
                   current_user.shopposts.build(shoppost_params)
@@ -7,16 +10,29 @@ class ShoppostsController < ApplicationController
                 end
     if @shoppost.save
       flash[:notice] = '投稿が完了しました'
-      redirect_to root_url
+      @shop = @shoppost.shop
+      redirect_to @shop
     else
       @shop = Shop.find(params[:id])
       render 'shops/show', status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @shop = @shoppost.shop
+    @shoppost.destroy
+    flash[:notice] = '投稿を削除しました'
+    redirect_to @shop, status: :see_other
+  end
+
   private
 
   def shoppost_params
     params.require(:shoppost).permit(:content, :shop_id)
+  end
+
+  def correct_user
+    @shoppost = current_user.shopposts.find_by(id: params[:id])
+    redirect_to root_url, status: :see_other if @shoppost.nil?
   end
 end
