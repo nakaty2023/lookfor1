faa30cda65b9053785c6c09d806b6abb8c3fcc5c
nakaty2023 <nakaty2023@example.com>
2023-09-03 16:19:39 +0900

    作成途中のスペックをコメント化

--- spec/requests/shopposts_spec.rb
+++ spec/requests/shopposts_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Shopposts', type: :request do
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/shopposts/new'
-      expect(response).to have_http_status(:success)
-    end
-  end
-end
+# RSpec.describe 'Shopposts', type: :request do
+#   describe 'GET /new' do
+#     it 'returns http success' do
+#       get '/shopposts/new'
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+# end
