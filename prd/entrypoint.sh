ef2adddc58fd3af962abc45d2c071f73b0a18e0d
nakaty2023 <nakaty2023@example.com>
2023-09-11 19:34:10 +0900

    本番環境での初期データ生成を確認したため、prd/entrypoint.shを修正

--- prd/entrypoint.sh
+++ prd/entrypoint.sh
@@ -2,9 +2,7 @@
 set -e
 
 cd /var/www
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 RAILS_ENV=production bundle exec rails db:migrate:reset
 RAILS_ENV=production bundle exec rails db:migrate
 RAILS_ENV=production bundle exec rails assets:precompile
-RAILS_ENV=production bundle exec rails db:seed
 
 exec "$@"
