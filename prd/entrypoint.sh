02b9d2970d13d68574f4206c18f9421ac48fd500
nakaty2023 <nakaty2023@example.com>
2023-09-11 16:52:30 +0900

    本番環境でDBリセット操作をする際の環境保護チェックを無効にするためprd/entrypoint.shを修正

--- prd/entrypoint.sh
+++ prd/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd /var/www
-RAILS_ENV=production bundle exec rails db:migrate:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 RAILS_ENV=production bundle exec rails db:migrate:reset
 RAILS_ENV=production bundle exec rails db:migrate
 RAILS_ENV=production bundle exec rails assets:precompile
 RAILS_ENV=production bundle exec rails db:seed
