982dc69b20f74394e2a5d80302869e570f89c171
nakaty2023 <nakaty2023@example.com>
2023-09-11 21:26:53 +0900

    本番環境でAWS（S3）に画像を保存できるよう設定 ・AWS用のgemを追加 ・ストレージオプションにAWSを追加

--- prd/entrypoint.sh
+++ prd/entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 cd /var/www
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 RAILS_ENV=production bundle exec rails db:migrate:reset
 RAILS_ENV=production bundle exec rails db:migrate
 RAILS_ENV=production bundle exec rails assets:precompile
+RAILS_ENV=production bundle exec rails db:seed
 
 exec "$@"
