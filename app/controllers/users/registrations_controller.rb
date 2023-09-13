5d731831c9514e2e94481eb9db5ec9dd02e1fc10
nakaty2023 <nakaty2023@example.com>
2023-09-13 14:15:40 +0900

    プロフィール編集ページの作成 ・ユーザー画像投稿機能実装 ・ユーザーのデフォルト画像を設定するモデルメソッドを追加 ・プロフィール編集ビューファイルの修正 ・日本語化対応のためロケールファイルを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,11 +46,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name gender age])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name gender age image])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name gender age])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name gender age image])
   end
 
   # The path used after sign up.
