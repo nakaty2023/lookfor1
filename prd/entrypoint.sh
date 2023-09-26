a501b871d60cdfda37a91d30d735e460f5b87fd8
nakaty2023 <nakaty2023@example.com>
2023-09-26 14:44:54 +0900

    ヘッダーの修正 ・ドロップダウンメニューが動作するよう、importmap.rb及びpackage.jsonを修正 ・ロゴ画像を追加

--- prd/entrypoint.sh
+++ prd/entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 cd /var/www
-RAILS_ENV=production bundle exec rails db:migrate:reset
+RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+RAILS_ENV=production bundle exec rails db:seed
 RAILS_ENV=production bundle exec rails assets:precompile
 
 exec "$@"
