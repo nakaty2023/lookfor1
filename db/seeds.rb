f06dee9b08bca13f65e4115a887c58842c293819
nakaty2023 <nakaty2023@example.com>
2023-10-11 14:04:39 +0900

    本番環境で初期データ（店舗に関する投稿、グッズ交換希望投稿、コメント）を作成するため、db/seeds.rbを修正
    ・その他
    本番環境で生成済みのデータに係るjsonファイルを削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,58 +10,35 @@
 #   user.save!
 # end
 
-# # Shops(id:18〜27)
-# #  Shop.where(id: 18..27).to_json(only: [:name, :address, :lat, :lon ], root: true)
-# #  上記実行結果 >> db/seeds/shops.json
-# shops_file = File.read(Rails.root.join('db', 'seeds', 'shops.json'))
-# shops_array = JSON.parse(shops_file)
-# shops_array.each do |shop|
-#   Shop.create!(shop['shop'])
-# end
-
-# # Items(id:96〜100)
-# #  Item.where(id: 96..100).to_json(only: [:name, :url], root: true)
-# #  上記実行結果 >> db/seeds/items.json
-# items_file = File.read(Rails.root.join('db', 'seeds', 'items.json'))
-# items_array = JSON.parse(items_file)
-# items_array.each do |item|
-#   Item.create!(item['item'])
-# end
-
-# # Shopitems
-# #  Shopitem.where(shop_id: 18..27).to_json(only: [:shop_id, :item_id], root: true)
-# #  上記実行結果 >> db/seeds/shopitems.json
-# shopitems_file = File.read(Rails.root.join('db', 'seeds', 'shopitems.json'))
-# shopitems_array = JSON.parse(shopitems_file)
-# shopitems_array.each do |shopitem|
-#   Shopitem.create!(shopitem['shopitem'])
-# end
-
 # Exchangeposts
-#  Exchangepost.all.to_json(only: [:give_item_name, :give_item_description, :want_item_name, :want_item_description, :place, user_id], root: true)
-#  上記実行結果 >> db/seeds/exchangeposts.json
 exchangeposts_file = File.read(Rails.root.join('db', 'seeds', 'exchangeposts.json'))
 exchangeposts_array = JSON.parse(exchangeposts_file)
 exchangeposts_array.each_with_index do |exchangepost, i|
-  exchangepost = Exchangepost.new(exchangepost['exchangepost'])
+  users = User.order(Arel.sql('RANDOM()')).limit(2)
+  user1 = users[0]
+  user2 = users[1]
+  exchangepost = user1.exchangeposts.build(exchangepost['exchangepost'])
   exchangepost.images.attach(io: File.open("./db/seeds/images/exchangepost_sample#{i}.jpg"), filename: "exchangepost_sample#{i}.jpg")
-  exchangepost.save!
-end
-
-# Comments
-comments_file = File.read(Rails.root.join('db', 'seeds', 'comments.json'))
-comments_array = JSON.parse(comments_file)
-comments_array.each do |comment|
-  Comment.create!(comment['comment'])
+  exchangepost.save
+  user2.comments.create(
+    content: "「＊＊＊＊フィギュア」であれば交換可能ですが、いかがでしょうか？",
+    exchangepost_id: Exchangepost.first.id)
+  user1.comments.create(
+    content: "ぜひ交換させていただきたいです！交換場所や交換日時の詳細はTwitter(＠****)で調整したいので、DMしていただけますか？",
+    exchangepost_id: Exchangepost.first.id)
+  user2.comments.create(
+    content: "ご返信ありがとうございます！早速DMしましたので、ご確認よろしくお願いします。",
+    exchangepost_id: Exchangepost.first.id)
 end
 
 # Shopposts
 50.times do
-  shoppost = Shoppost.new(
-    content: "○月○日の○時頃に店舗へ行きましたが、呪術廻戦 懐玉・玉折 ～弐～の▲賞、*賞の在庫ありました！在庫状況のポスターは下記の通りでした！",
-    user_id: rand(1..4),
-    shop_id: rand(1..607),
+  user = User.order(Arel.sql('RANDOM()')).first
+  shop = Shop.order(Arel.sql('RANDOM()')).first
+  shoppost = user.shopposts.build(
+    content: "○月○日の○時頃に店舗へ行きましたが、鬼滅の刃 ～襲撃～の▲賞、*賞の在庫ありました！在庫状況のポスターは下記の通りでした！",
+    shop_id: shop.id,
   )
   shoppost.images.attach(io: File.open("./db/seeds/images/sample_image.jpeg"), filename: 'sample_image.jpeg')
-  shoppost.save!
+  shoppost.save
 end
