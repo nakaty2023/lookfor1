5aa9c215962e2111af3692c46cfcefe70e303119
nakaty2023 <nakaty2023@example.com>
2023-10-16 20:52:57 +0900

    マイグレーションの適用

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,7 @@
 class Conversation < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :recipient, class_name: 'User'
+  has_many :messages, dependent: :destroy
   validates_uniqueness_of :sender_id, scope: :recipient_id
 
   def self.between(sender_id, recipient_id)
