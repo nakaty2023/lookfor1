aac0d74bef031ea38d0a3f198a1620eb91d4a3ad
nakaty2023 <nakaty2023@example.com>
2023-10-16 22:16:31 +0900

    messagesコントローラーの作成

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,28 @@
+class MessagesController < ApplicationController
+  before_action :find_conversation
+
+  def index
+    @messages = @conversation.messages
+    @message = @conversation.messages.new
+  end
+
+  def create
+    @message = @conversation.messages.new(message_params)
+    @message.user = current_user
+    if @message.save
+      redirect_to conversation_messages_path(@conversation)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def find_conversation
+    @conversation = Conversation.find(params[:conversation_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+end
