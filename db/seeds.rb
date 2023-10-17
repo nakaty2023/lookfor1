2ae75f185f4e91fabdc6f9141519c83d2330839f
nakaty2023 <nakaty2023@example.com>
2023-10-18 08:23:48 +0900

    会話とメッセージの初期データ作成のため、seeds.rbを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
 #   user.save!
 # end
 
-# Exchangeposts
+# Exchangeposts, Comments, Conversation, Message
 exchangeposts_file = File.read(Rails.root.join('db', 'seeds', 'exchangeposts.json'))
 exchangeposts_array = JSON.parse(exchangeposts_file)
 exchangeposts_array.each_with_index do |exchangepost, i|
@@ -21,24 +21,34 @@ exchangeposts_array.each_with_index do |exchangepost, i|
   exchangepost.images.attach(io: File.open("./db/seeds/images/exchangepost_sample#{i}.jpg"), filename: "exchangepost_sample#{i}.jpg")
   exchangepost.save
   user2.comments.create(
-    content: "「＊＊＊＊フィギュア」であれば交換可能ですが、いかがでしょうか？",
+    content: "「*******フィギュア」であれば交換可能ですが、いかがでしょうか？",
     exchangepost_id: Exchangepost.first.id)
   user1.comments.create(
-    content: "ぜひ交換させていただきたいです！交換場所や交換日時の詳細はTwitter(＠****)で調整したいので、DMしていただけますか？",
+    content: "ぜひ交換させていただきたいです！交換場所や交換日時の詳細はDMしていただけますか？",
     exchangepost_id: Exchangepost.first.id)
   user2.comments.create(
     content: "ご返信ありがとうございます！早速DMしましたので、ご確認よろしくお願いします。",
     exchangepost_id: Exchangepost.first.id)
+  conversation = Conversation.between(user1.id, user2.id)
+  conversation.messages.create(
+    body: "*******フィギュアの交換場所は▲▲▲▲▲でいかがでしょうか？交換日時は○月○日〜○月○日の○○時以降の時間帯でお願いしたいです。",
+    user_id: user2.id)
+  conversation.messages.create(
+    body: "DMありがとうございます。交換場所は▲▲▲▲▲でOKです！交換日時は○月○日の○○時でお願いします。",
+    user_id: user1.id)
+  conversation.messages.create(
+    body: "ご返信ありがとうございました。日時と場所について、承知しました！当日はどうぞよろしくお願いいたします。",
+    user_id: user2.id)
 end
 
-# Shopposts
-50.times do
-  user = User.order(Arel.sql('RANDOM()')).first
-  shop = Shop.order(Arel.sql('RANDOM()')).first
-  shoppost = user.shopposts.build(
-    content: "○月○日の○時頃に店舗へ行きましたが、鬼滅の刃 ～襲撃～の▲賞、*賞の在庫ありました！在庫状況のポスターは下記の通りでした！",
-    shop_id: shop.id,
-  )
-  shoppost.images.attach(io: File.open("./db/seeds/images/sample_image.jpeg"), filename: 'sample_image.jpeg')
-  shoppost.save
-end
+# # Shopposts
+# 50.times do
+#   user = User.order(Arel.sql('RANDOM()')).first
+#   shop = Shop.order(Arel.sql('RANDOM()')).first
+#   shoppost = user.shopposts.build(
+#     content: "○月○日の○時頃に店舗へ行きましたが、鬼滅の刃 ～襲撃～の▲賞、*賞の在庫ありました！在庫状況のポスターは下記の通りでした！",
+#     shop_id: shop.id,
+#   )
+#   shoppost.images.attach(io: File.open("./db/seeds/images/sample_image.jpeg"), filename: 'sample_image.jpeg')
+#   shoppost.save
+# end
